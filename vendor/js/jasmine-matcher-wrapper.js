2eea6e031ebffe849f678613e617843f70fd8e04
Justin Searls <searls@gmail.com>
2014-01-21 18:15:29 -0500

    Fixed remaining specs by improving matcher wrapper
    10/10 regression tests passing.

--- vendor/js/jasmine-matcher-wrapper.js
+++ vendor/js/jasmine-matcher-wrapper.js
@@ -1,18 +1,40 @@
-/* jasmine-matcher-wrapper - 0.0.1
+/* jasmine-matcher-wrapper - 0.0.2
  * Wraps Jasmine 1.x matchers for use with Jasmine 2
  * https://github.com/testdouble/jasmine-matcher-wrapper
  */
 (function() {
-  var __hasProp = {}.hasOwnProperty,
-    __slice = [].slice;
+  var __slice = [].slice,
+    __hasProp = {}.hasOwnProperty;
 
   (function(jasmine) {
+    var comparatorFor;
     if (jasmine == null) {
       return typeof console !== "undefined" && console !== null ? console.warn("jasmine was not found. Skipping jasmine-matcher-wrapper. Verify your script load order.") : void 0;
     }
     if (jasmine.matcherWrapper != null) {
       return;
     }
+    comparatorFor = function(matcher, isNot) {
+      return function() {
+        var actual, context, message, params, pass, _ref;
+        actual = arguments[0], params = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+        context = {
+          actual: actual,
+          isNot: isNot
+        };
+        pass = matcher.apply(context, params);
+        if (isNot) {
+          pass = !pass;
+        }
+        if (!pass) {
+          message = (_ref = context.message) != null ? _ref.apply(context, params) : void 0;
+        }
+        return {
+          pass: pass,
+          message: message
+        };
+      };
+    };
     return jasmine.matcherWrapper = {
       wrap: function(matchers) {
         var matcher, name, wrappedMatchers;
@@ -25,21 +47,8 @@
           matcher = matchers[name];
           wrappedMatchers[name] = function() {
             return {
-              compare: function() {
-                var actual, context, message, params, pass, _ref;
-                actual = arguments[0], params = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
-                context = {
-                  actual: actual
-                };
-                pass = matcher.apply(context, params);
-                if (!pass) {
-                  message = (_ref = context.message) != null ? _ref.apply(context, params) : void 0;
-                }
-                return {
-                  pass: pass,
-                  message: message
-                };
-              }
+              compare: comparatorFor(matcher, false),
+              negativeCompare: comparatorFor(matcher, true)
             };
           };
         }
